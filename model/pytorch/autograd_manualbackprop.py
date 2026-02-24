abfc06b1eeac53298b295eea98204592d167bd3f
Sanjay Kumar Sakamuri Kamalakar <sksanjaykumar010307@gmail.com>
2026-02-24 22:42:02 +0530

    build a tiny regression model manually

--- model/pytorch/autograd_manualbackprop.py
+++ model/pytorch/autograd_manualbackprop.py
@@ -1,9 +1,20 @@
 import torch
 # Input and target
-
 x = torch.tensor([2.0])
 y_true = torch.tensor([8.0])
 
 # parameters
 w = torch.tensor([1.0], requires_grad=True) # requires_grad=True to compute gradients
 b = torch.tensor([0.0], requires_grad=True)
+
+# forward pass: compute predicted y
+y_pred = w * x + b
+
+# Loss function
+loss = (y_pred - y_true) ** 2
+
+# backward pass: compute gradients
+loss.backward()
+
+print("Gradienty of w:", w.grad)  # Gradient of w
+print("Gradienty of b:", b.grad)  # Gradient of b
