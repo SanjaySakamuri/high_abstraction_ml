56f77a7cdda498c8520c0003df097abd6723a766
Sanjay Kumar Sakamuri Kamalakar <sksanjaykumar010307@gmail.com>
2026-02-24 22:39:14 +0530

    Input Output and Parameters

--- model/pytorch/autograd_manualbackprop.py
+++ model/pytorch/autograd_manualbackprop.py
@@ -1,1 +1,9 @@
+import torch
+# Input and target
 
+x = torch.tensor([2.0])
+y_true = torch.tensor([8.0])
+
+# parameters
+w = torch.tensor([1.0], requires_grad=True) # requires_grad=True to compute gradients
+b = torch.tensor([0.0], requires_grad=True)
