9733d96d2c558787a15dacea86770f046610447e
Sanjay Kumar Sakamuri Kamalakar <sksanjaykumar010307@gmail.com>
2026-03-08 22:39:26 +0530

    Implement seed function for reproducibility
    Added seed setting function for reproducibility.

--- model/pytorch/BCEWithLogitsLoss.py
+++ model/pytorch/BCEWithLogitsLoss.py
@@ -1,1 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
 
+# Reproducibility
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
