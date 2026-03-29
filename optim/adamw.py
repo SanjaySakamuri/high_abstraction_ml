00065145786e0d8a9899f538508db6b7a8996fd7
Sanjay Kumar Sakamuri Kamalakar <sksanjaykumar010307@gmail.com>
2026-03-29 23:12:39 +0530

    Implement AdamW optimizer class

--- optim/adamw.py
+++ optim/adamw.py
@@ -1,1 +1,14 @@
+import numpy as np
+
+class AdamW:
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=1e-2):
+        self.params = params
+        self.lr = lr
+        self.beta1, self.beta2 = betas
+        self.eps = eps
+        self.wd = weight_decay
+
+        self.m = [np.zeros_like(p) for p in params]
+        self.v = [np.zeros_like(p) for p in params]
+        self.t = 0
 
