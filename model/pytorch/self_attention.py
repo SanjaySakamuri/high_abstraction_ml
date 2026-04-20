f9b74b957e6b3b739921b4dd3794465717542320
Sanjay Kumar Sakamuri Kamalakar <sksanjaykumar010307@gmail.com>
2026-04-20 23:14:31 +0530

    Add SelfAttention class to self_attention.py

--- model/pytorch/self_attention.py
+++ model/pytorch/self_attention.py
@@ -1,1 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+class SelfAttention(nn.Module):
+  def __init__(self, d_model):
+      super().__init__()
+      self.d_model = d_model
