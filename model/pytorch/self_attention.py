8910336c7d83299f30aec73d386267204e58bfff
Sanjay Kumar Sakamuri Kamalakar <sksanjaykumar010307@gmail.com>
2026-04-20 23:15:56 +0530

    Add linear layers for Q, K, V in SelfAttention

--- model/pytorch/self_attention.py
+++ model/pytorch/self_attention.py
@@ -6,3 +6,13 @@ class SelfAttention(nn.Module):
   def __init__(self, d_model):
       super().__init__()
       self.d_model = d_model
+
+      # Q, K, V
+      self.W_q = nn.Linear(d_model, d_model, bias=False)
+      self.W_k = nn.Linear(d_model, d_model, bias=False)
+      self.W_v = nn.Linear(d_model, d_model, bias=False)
+
+      # Output
+      self.W_o = nn.Linear(d_model, d_model)
+
+  
