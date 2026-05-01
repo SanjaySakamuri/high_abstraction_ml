c17e9be94e8060936176876340e47ee714659eba
Sanjay Kumar Sakamuri Kamalakar <sksanjaykumar010307@gmail.com>
2026-05-01 23:21:30 +0530

    Add LogisticRegression class with sigmoid method

--- model/pytorch/LogisticRegression.py
+++ model/pytorch/LogisticRegression.py
@@ -1,1 +1,13 @@
+import numpy as np
+
+class LogisticRegression:
+    def __init__(self, lr=0.01, epochs=1000):
+        self.lr = lr
+        self.epochs = epochs
+        self.weights = None
+        self.bias = None
+
+class sigmoid(self, z):
+  return 1 / (1 + np.exp(-z)) # Maps any real value into a range between 0 and 1
+
 
